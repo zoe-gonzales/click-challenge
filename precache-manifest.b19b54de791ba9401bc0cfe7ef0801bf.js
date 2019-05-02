3465cd235399b9b845ca5147249637329a0cd236
zoe-gonzales <zoegonzales03@gmail.com>
2019-05-01 19:48:25 -0600

    Updates

---
+++ precache-manifest.b19b54de791ba9401bc0cfe7ef0801bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc74fe02df2367cdff3b2519e33f5f94",
+    "url": "/click-challenge/index.html"
+  },
+  {
+    "revision": "de48b8b9638b047a51f3",
+    "url": "/click-challenge/static/css/main.cda83451.chunk.css"
+  },
+  {
+    "revision": "5888186bdf395e655ef0",
+    "url": "/click-challenge/static/js/2.59debe72.chunk.js"
+  },
+  {
+    "revision": "de48b8b9638b047a51f3",
+    "url": "/click-challenge/static/js/main.8cb8b6ca.chunk.js"
+  },
+  {
+    "revision": "ed1ce67b0f2059523d24",
+    "url": "/click-challenge/static/js/runtime~main.f1087261.js"
+  }
+]);
